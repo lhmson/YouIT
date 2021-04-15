508e9c647e001fbba3c14b6ca6ea4ec8a3411116
Ngo Cong Hau <muiheoconghau@gmail.com>
2021-04-15 14:37:57 +0700

    Color constants

--- client/src/constants/colors.js
+++ client/src/constants/colors.js
@@ -1,7 +1,15 @@
 const COLOR = {
-  black: "#333",
-  green: "#27ae60",
+  black: "#333333",
   white: "#fff",
+  whiteSmoke: "F2F2F2",
+  gray: "828282",
+  green: "#27AE60",
+  darkGreen: "219653",
+  lightGreen: "6FCF97",
+  greenSmoke: "DBF3E5",
+  blue: "2F80ED",
+  red: "EB5757",
+  yellow: "F2C94C",
 };
 
 export default COLOR;
