0a3e02eb075268110a65d4af6ab03e50796a54e9
LaNeitD18 <phanhuytien14@gmail.com>
2021-07-07 01:09:28 +0700

    Fix programming pane

--- client/src/components/UserInfo/AboutCard/ProgrammingPane/ProgrammingPane.js
+++ client/src/components/UserInfo/AboutCard/ProgrammingPane/ProgrammingPane.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Button, Input, Layout, Row, Tag } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Col, Input, Layout, Row, Select, Tag } from "antd";
 
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./styles.js";
@@ -7,33 +7,42 @@ import styles from "./styles.js";
 import * as apiHashtag from "../../../../api/hashtag";
 import * as apiUser from "../../../../api/user_info";
 import { isLoginUser } from "../../../../utils/user.js";
+import { fetchHashtags } from "../../../../redux/actions/hashtag.js";
+
+const { Option } = Select;
 
 function ProgrammingPane() {
   const user = useSelector((state) => state.user);
+  const listHashtags = useSelector((state) => state.hashtags);
+
   const dispatch = useDispatch();
   const isLogin = isLoginUser(user);
 
   const [isAdding, setIsAdding] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const [listHashtags, setListHashtags] = useState([]);
+  const [inputHashtags, setInputHashtags] = useState([]);
+  const [listProgrammingHashtags, setListProgrammingHashtags] = useState([]);
 
   const fetchProgrammingHashtags = async () => {
-    const hashtags = await (
-      await apiHashtag.fetchProgrammingHashtags(user?._id)
-    ).data;
-    setListHashtags(hashtags);
+    const hashtags = listHashtags.filter((tag) =>
+      user?.userInfo?.programmingHashtags.includes(tag?._id)
+    );
+    setListProgrammingHashtags(hashtags);
   };
 
   useEffect(() => {
-    fetchProgrammingHashtags();
+    dispatch(fetchHashtags());
   }, []);
+
+  useEffect(() => {
+    fetchProgrammingHashtags();
+  }, [listHashtags]);
 
   const removeHashtag = async (removedHashtagId) => {
     // front
-    const hashtags = listHashtags.filter(
+    const hashtags = listProgrammingHashtags.filter(
       (tag) => tag?._id !== removedHashtagId
     );
-    setListHashtags(hashtags);
+    setListProgrammingHashtags(hashtags);
 
     // back
     await apiUser.removeProgrammingHashtag(removedHashtagId);
@@ -41,28 +50,33 @@ function ProgrammingPane() {
   };
 
   const handleInputConfirm = async () => {
-    if (inputValue && listHashtags.indexOf(inputValue) === -1) {
-      // back
-      const newHashtag = {
-        name: inputValue,
-      };
-      const { data } = await apiHashtag.createHashtag(newHashtag);
-      await apiUser.addProgrammingHashtag(data?._id);
+    const tagNames = listProgrammingHashtags.map((tag) => tag?.name);
+    const addedHashtags = [];
 
-      // front
-      setListHashtags([...listHashtags, data]);
-      setIsAdding(false);
-      setInputValue("");
-      console.log(data);
+    for (const hashtag of inputHashtags) {
+      if (hashtag && tagNames.indexOf(hashtag) === -1) {
+        // back
+        const newHashtag = {
+          name: hashtag,
+        };
+        const { data } = await apiHashtag.createHashtag(newHashtag);
+        await apiUser.addProgrammingHashtag(data?._id);
+
+        // front
+        addedHashtags.push(data);
+      }
     }
+    setListProgrammingHashtags([...listProgrammingHashtags, ...addedHashtags]);
+    setIsAdding(false);
+    setInputHashtags([]);
   };
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
+  // const handleInputChange = (e) => {
+  //   setInputValue(e.target.value);
+  // };
 
   const TagItems = () => {
-    return listHashtags.map((hashtag) => {
+    return listProgrammingHashtags.map((hashtag) => {
       return (
         <span
           key={hashtag?._id}
@@ -87,13 +101,30 @@ function ProgrammingPane() {
     return (
       <div>
         {isAdding ? (
-          <Input
-            style={{ width: "30%" }}
-            onPressEnter={handleInputConfirm}
-            onBlur={handleInputConfirm}
-            onChange={handleInputChange}
-            autoFocus={true}
-          ></Input>
+          <Col>
+            <Select
+              mode="tags"
+              placeholder="Select hashtags"
+              onChange={setInputHashtags}
+              style={{ width: "90%" }}
+            >
+              {listHashtags.map((tag, i) => (
+                <Option key={tag?.name ?? i}>{tag?.name}</Option>
+              ))}
+            </Select>
+            <Row>
+              <Button
+                className="green-button"
+                style={styles.button}
+                onClick={handleInputConfirm}
+              >
+                Save
+              </Button>
+              <Button style={styles.button} onClick={() => setIsAdding(false)}>
+                Cancel
+              </Button>
+            </Row>
+          </Col>
         ) : (
           <Button className="green-button" onClick={() => setIsAdding(true)}>
             New hashtag
