4c160b8b8ebd388c3c6185527dcac7f007ff190a
sanhlike <18520146@gm.uit.edu.vn>
2021-07-08 23:00:31 +0700

    finish UI

--- client/src/api/group.js
+++ client/src/api/group.js
@@ -1,7 +1,6 @@
 import API from "./index";
 
 export const createGroup = (newGroup) => API.post("/group", newGroup);
-
 export const fetchAGroup = (id) => API.get(`/group/${id}`);
 export const fetchUserJoinedGroups = () => API.get(`/group/list/joinedByMe`);
 export const fetchCountGroups = (range, timeString) =>
